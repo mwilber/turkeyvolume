a352887c63f898e8dd805915c992618056fb6c22
Matt Wilber <mwilber@gmail.com>
2015-04-08 11:27:59 -0400

    Finish up 3d cube effect. Fix bug in depth tab alignment. Prevent dragging cube out of bounds.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var EXTRUDE_MULTIPLIER = 50;
+var OUTER_MARGIN = 10;
+var F_COLOR = "#000000";
+var M_COLOR = "#333333";
+var B_COLOR = "#666666";
+
 var canvas, stage;
 
 var mouseTarget;	// the display object currently under the mouse, or being dragged
@@ -5,12 +11,13 @@ var dragStarted;	// indicates whether we are currently in a drag operation
 var offset;
 var update = true;
 
-var fface, bface, tabh, tabw, tabd, tabp;
+var fface, bface, mface, tabh, tabw, tabd, tabp;
 var dscale = 1;
 
 $(document).ready(function(){
 
     stage = new createjs.Stage("volumizer");
+    stage.setBounds(0,0,$('#volumizer').width(), $('#volumizer').height())
 
 	// enable touch interactions if supported on the current device:
 	createjs.Touch.enable(stage);
@@ -20,6 +27,13 @@ $(document).ready(function(){
 	stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
 
     FaceInitB();
+
+    mface = new createjs.Shape();
+    mface.x = 0;
+    mface.y = 0;
+
+    stage.addChild(mface);
+
     FaceInitF();
     TabInitH();
     TabInitW();
@@ -36,7 +50,7 @@ $(document).ready(function(){
 
 function FaceInitF(){
     fface = new createjs.Shape();
-    fface.graphics.beginStroke("Black").drawRect(0, 0, 300, 300);
+    fface.graphics.beginStroke(F_COLOR).drawRect(0, 0, 300, 300);
     fface.setBounds(0,0,300,300);
     fface.x = 100;
     fface.y = 100;
@@ -48,7 +62,7 @@ function FaceInitF(){
 
 function FaceInitB(){
     bface = new createjs.Shape();
-    bface.graphics.beginStroke("Black").drawRect(0, 0, 300, 300);
+    bface.graphics.beginStroke(B_COLOR).drawRect(0, 0, 300, 300);
     bface.setBounds(0,0,300,300);
     bface.x = 100;
     bface.y = 100;
@@ -60,26 +74,50 @@ function FaceInitB(){
 
 function UpdateCubeP(){
 
+    // Set the face positions
+    var xoffset = ((stage.getBounds().width/2)-(stage.getBounds().width-tabp.x))/(stage.getBounds().width/2);
+    var yoffset = ((stage.getBounds().height/2)-(stage.getBounds().height-tabp.y))/(stage.getBounds().height/2);
+    //console.log('xoffset',xoffset);
     //console.log(fface.graphics.getBounds().width);
     var fb = fface.getTransformedBounds();
+    var bb = bface.getTransformedBounds();
 
     fface.x = tabp.x - (fb.width/2);
     fface.y = tabp.y - (fb.height/2);
 
+    bface.x = tabp.x - (bb.width/2) - (xoffset*EXTRUDE_MULTIPLIER);
+    bface.y = tabp.y - (bb.height/2) - (yoffset*EXTRUDE_MULTIPLIER);
+
+    // Draw out the connecting lines
+    mface.graphics.clear().beginStroke(M_COLOR)
+        .moveTo(fface.x, fface.y)
+        .lineTo(bface.x, bface.y)
+        .moveTo(fface.x+fface.getTransformedBounds().width, fface.y)
+        .lineTo(bface.x+bface.getTransformedBounds().width, bface.y)
+        .moveTo(fface.x, fface.y+fface.getTransformedBounds().height)
+        .lineTo(bface.x, bface.y+bface.getTransformedBounds().height)
+        .moveTo(fface.x+fface.getTransformedBounds().width, fface.y+fface.getTransformedBounds().height)
+        .lineTo(bface.x+bface.getTransformedBounds().width, bface.y+bface.getTransformedBounds().height);
+
+    // Position the control tabs
     tabh.x = fface.x + (fb.width/2);
     tabh.y = fface.y;
 
     tabw.x = fface.x;
     tabw.y = fface.y + (fb.height/2);
 
-    tabd.x = fface.x + fb.width + ( (fface.x + fb.width) - ((fface.x + fb.width)*dscale));
+    console.log((dscale));
+
+    //tabd.x = fface.x + fb.width - ((fb.width)*(1-dscale));// + ( (fface.x + fb.width) - ((fface.x + fb.width)*dscale));
+    tabd.x = fface.x + (fb.width*dscale);
     tabd.y = fface.y + (fb.height);
+
 }
 
 function UpdateCubeH(){
     //console.log( ((fface.y+(fface.getTransformedBounds().height/2))-this.y)/(fface.getBounds().height/2) );
     fface.scaleY = (((fface.y+(fface.getTransformedBounds().height/2))-tabh.y)/(fface.getBounds().height/2));
-    bface.scaleY = (fface.scaleY)*(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
+    bface.scaleY = (fface.scaleY)/(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
     //console.log(fface.getBounds().height);
     //console.log((((fface.y+(fface.getTransformedBounds().height/2))-tabh.y)/(fface.getBounds().height/2)));
 
@@ -88,15 +126,20 @@ function UpdateCubeH(){
 
 function UpdateCubeW(){
     fface.scaleX = (((fface.x+(fface.getTransformedBounds().width/2))-tabw.x)/(fface.getBounds().width/2));
-    bface.scaleX = (fface.scaleX)*(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
+    bface.scaleX = (fface.scaleX)/(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
     UpdateCubeP();
 }
 
 function UpdateCubeD(){
     //console.log(fface.x, (fface.getTransformedBounds().width),  tabd.x, (fface.getBounds().width/2));
     //console.log( 1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width/2)) );
-    dscale = (1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
-    console.log(dscale);
+    //console.log( ( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width/2))+1 );
+
+
+
+    //dscale = (1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
+    dscale = ( ((tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getTransformedBounds().width))+1 );
+    //console.log(dscale);
     bface.scaleX = (fface.scaleX)*dscale;
     bface.scaleY = (fface.scaleY)*dscale;
     UpdateCubeP();
@@ -196,8 +239,17 @@ function TabInitP(){
 
 	// the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
     tabp.on("pressmove", function (evt) {
-		this.x = evt.stageX + this.offset.x;
-		this.y = evt.stageY + this.offset.y;
+
+        if( (evt.stageX + this.offset.x) > (stage.getBounds().x+(fface.getBounds().x+(fface.getTransformedBounds().width/2))+OUTER_MARGIN) &&
+            (evt.stageX + this.offset.x) < (stage.getBounds().width-(fface.getBounds().x+(fface.getTransformedBounds().width/2))-OUTER_MARGIN)
+        ){
+    		this.x = evt.stageX + this.offset.x;
+        }
+        if( (evt.stageY + this.offset.y) > (stage.getBounds().y+(fface.getBounds().y+(fface.getTransformedBounds().height/2))+OUTER_MARGIN) &&
+            (evt.stageY + this.offset.y) < (stage.getBounds().height-(fface.getBounds().y+(fface.getTransformedBounds().height/2))-OUTER_MARGIN)
+        ){
+            this.y = evt.stageY + this.offset.y;
+        }
 		// indicate that the stage should be updated on the next tick:
 		update = true;
 
