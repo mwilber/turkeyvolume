9366156c1a3351a37ad528705389f7b10ebde249
Matt Wilber <mwilber@gmail.com>
2015-04-07 22:19:49 -0400

    Add depth tab

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var dragStarted;	// indicates whether we are currently in a drag operation
 var offset;
 var update = true;
 
-var fface, tabh, tabw, tabd, tabp
+var fface, bface, tabh, tabw, tabd, tabp;
+var dscale = 1;
 
 $(document).ready(function(){
 
@@ -18,10 +19,14 @@ $(document).ready(function(){
 	stage.enableMouseOver(10);
 	stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
 
+    FaceInitB();
+    FaceInitF();
     TabInitH();
     TabInitW();
     TabInitP();
-    FaceInitF();
+    TabInitD();
+
+
 
     createjs.Ticker.addEventListener("tick", tick);
 
@@ -41,6 +46,18 @@ function FaceInitF(){
     stage.update();
 }
 
+function FaceInitB(){
+    bface = new createjs.Shape();
+    bface.graphics.beginStroke("Black").drawRect(0, 0, 300, 300);
+    bface.setBounds(0,0,300,300);
+    bface.x = 100;
+    bface.y = 100;
+
+    stage.addChild(bface);
+
+    stage.update();
+}
+
 function UpdateCubeP(){
 
     //console.log(fface.graphics.getBounds().width);
@@ -54,11 +71,15 @@ function UpdateCubeP(){
 
     tabw.x = fface.x;
     tabw.y = fface.y + (fb.height/2);
+
+    tabd.x = fface.x + fb.width + ( (fface.x + fb.width) - ((fface.x + fb.width)*dscale));
+    tabd.y = fface.y + (fb.height);
 }
 
 function UpdateCubeH(){
     //console.log( ((fface.y+(fface.getTransformedBounds().height/2))-this.y)/(fface.getBounds().height/2) );
     fface.scaleY = (((fface.y+(fface.getTransformedBounds().height/2))-tabh.y)/(fface.getBounds().height/2));
+    bface.scaleY = (fface.scaleY)*(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
     //console.log(fface.getBounds().height);
     //console.log((((fface.y+(fface.getTransformedBounds().height/2))-tabh.y)/(fface.getBounds().height/2)));
 
@@ -67,6 +88,17 @@ function UpdateCubeH(){
 
 function UpdateCubeW(){
     fface.scaleX = (((fface.x+(fface.getTransformedBounds().width/2))-tabw.x)/(fface.getBounds().width/2));
+    bface.scaleX = (fface.scaleX)*(1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
+    UpdateCubeP();
+}
+
+function UpdateCubeD(){
+    //console.log(fface.x, (fface.getTransformedBounds().width),  tabd.x, (fface.getBounds().width/2));
+    //console.log( 1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width/2)) );
+    dscale = (1-( (tabd.x-(fface.x+fface.getTransformedBounds().width))/(fface.getBounds().width)));
+    console.log(dscale);
+    bface.scaleX = (fface.scaleX)*dscale;
+    bface.scaleY = (fface.scaleY)*dscale;
     UpdateCubeP();
 }
 
@@ -124,8 +156,8 @@ function TabInitW(){
 function TabInitD(){
     tabd = new createjs.Shape();
     tabd.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 25);
-    tabd.x = 250;
-    tabd.y = 100;
+    tabd.x = 400;
+    tabd.y = 400;
 
     stage.addChild(tabd);
 
@@ -133,15 +165,16 @@ function TabInitD(){
 	// in this case that means it executes in the scope of the button.
     tabd.on("mousedown", function (evt) {
 		//this.parent.addChild(this);
-		this.offset = {x: this.x, y: this.y - evt.stageY};
+		this.offset = {x: this.x - evt.stageX, y: this.y};
 	});
 
 	// the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
     tabd.on("pressmove", function (evt) {
-		//this.x = evt.stageX + this.offset.x;
-		this.y = evt.stageY + this.offset.y;
+		this.x = evt.stageX + this.offset.x;
+		//this.y = evt.stageY + this.offset.y;
 		// indicate that the stage should be updated on the next tick:
 		update = true;
+        UpdateCubeD();
 	});
 }
 
