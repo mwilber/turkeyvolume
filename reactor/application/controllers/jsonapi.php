b484abea9fe123714f9f745f7638cb2ddefa0349
Matt Wilber <mwilber@gmail.com>
2015-04-20 16:14:05 -0400

    trying jsonapi this time

--- reactor/application/controllers/jsonapi.php
+++ reactor/application/controllers/jsonapi.php
@@ -20,6 +20,7 @@ class JSONAPI extends CI_Controller {
 
 	function _JSONout(){
 		header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 		header('Content-type: application/json');
 		echo json_encode($this->_response);
 	}
