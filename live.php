81baf872292fedb588e7b74e6667ae16e306bb14
Matt Wilber <mwilber@gmail.com>
2015-05-03 22:46:39 -0400

    Set up layout for home page

---
+++ live.php
@@ -0,0 +1,114 @@
+<?php
+
+	$listTitle = "Turkey Volume Guessing App";
+
+	$social = array();
+	$social['title'] = "Turkey Volume Guessing App";
+	$social['description'] = "From the creator of JAWS Converter. An app that takes animal based measurement into the third dimension. Coming November 2015.";
+	$social['image'] = "http://www.turkeyvolumeguessingapp.com/img/social.gif";
+	$social['link'] = "http://www.turkeyvolumeguessingapp.com".$_SERVER[REQUEST_URI];
+?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title><?=$pgTitle?></title>
+		<meta name="description" content="<?=$social['description']?>">
+		<meta name="author" content="Matthew Wilber">
+		<meta property="og:title" content="<?=$social['title']?>" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="<?=$social['link']?>" />
+		<meta property="og:image" content="<?=$social['image']?>" />
+		<meta property="og:site_name" content="<?=$social['title']?>" />
+		<meta property="fb:admins" content="631337813" />
+		<meta property="og:description" content="<?=$social['description']?>" />
+
+
+		<!-- Twitter Summary Card -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@greenzeta">
+		<meta name="twitter:title" content="<?=$social['title']?>">
+		<meta name="twitter:description" content="<?=$social['description']?>">
+		<meta name="twitter:creator" content="@greenzeta">
+		<meta name="twitter:image:src" content="<?=$social['image']?>">
+		<meta name="twitter:domain" content="turkeyvolumeguessingapp.com">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <div id="content">
+                    <div id="logo">
+                        <h1>Turkey Volume Guessing App</h1>
+            		  <img src="img/logo_512.png"/>
+                      
+                    </div>
+    	        	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent enim libero, tincidunt ac vulputate eget, tristique sit amet massa. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+    				<a id="playstore" style="float:left; margin-right:20px; margin-left:100px;" href="https://play.google.com/store/apps/details?id=com.greenzeta.greenzeta.hoursaround" target="_blank"><img src="img/playstore.png" style="height:45px;"/></a>
+    				<a id="playstore" style="float:left; clear:none;" href="https://itunes.apple.com/us/app/hours-around/id650401729?ls=1&mt=8" target="_blank"><img src="img/appstore.png" style="height:45px;"/></a>
+    			</div>
+    	        <div id="eyepad">
+    	        	<iframe src="app/index.html" frameborder="0"></iframe>
+    	        </div>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div id="footer">
+              <a href="policy.php" onclick="ga('send', 'event', 'web', 'click', 'policy', 0);" class="policy">
+            Privacy Policy
+          </a>
+              <a href="#" onclick="window.open('http://www.greenzeta.com/home/listing/product', '_system'); ga('send', 'event', 'web', 'click', 'GreenZeta', 0); return false;" class="gz">
+            <span class="badge">&zeta;</span>
+            &nbsp;&nbsp;A GreenZeta Production
+          </a>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+               $('.animation form').submit(function(){
+                   $.post('./reactor/jsonapi/register',{email:$('#email').val()},function(){
+                       $('.animation form').empty().append($('<label>').html("Thank You."));
+                   });
+                  return false;
+               });
+            });
+        </script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '<?php if($_SERVER["HTTP_HOST"] != "gibson.loc"): ?>UA-76054-32<?php endif; ?>']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', '<?php if($_SERVER["HTTP_HOST"] != "gibson.loc"): ?>UA-76054-32<?php endif; ?>', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+    </body>
+</html>
